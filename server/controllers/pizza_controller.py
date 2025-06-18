5731195ce834c8f9f25ba6bd3c71047a054ce2ef
Rodney <rodney.amani@student.moringaschool.com>
2025-06-18 14:26:35 +0300

    pizza controller updated

---
+++ server/controllers/pizza_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+from server.models.pizza import Pizza
+
+pizza_bp = Blueprint("pizza_bp", __name__)
+
+@pizza_bp.route("/pizzas", methods=["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([{
+        "id": p.id,
+        "name": p.name,
+        "ingredients": p.ingredients
+    } for p in pizzas]), 200
